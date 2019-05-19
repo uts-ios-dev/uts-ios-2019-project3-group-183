b8123d6f4901e5f82c187ce77e47016071191132
Aryan Nateq <_>
2019-05-19 22:09:05 +1000

    Initialisation of our Cryptocurrency app project called Cryptopia.
    Setup tabview.
    Setup 3 new view controllers - one for portfolio, another for browse, and another for analysis.
    
    Setup a dark colour palette to use as the base colour design for our app.

---
+++ Cryptopia/Cryptopia/View Controllers/PortfolioViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cryptopia
+//
+//  Created by SuperUser on 19/5/19.
+//  Copyright © 2019 UTS_iOSDev. All rights reserved.
+//
+
+import UIKit
+
+class PortfolioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
