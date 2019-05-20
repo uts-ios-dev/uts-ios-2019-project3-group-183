4bb12f55c5cb07095fac9c6666c3f14d25aacd9d
Aryan Nateq <_>
2019-05-21 02:42:48 +1000

    Added a UITableView and accompanying UITableCells containing info about the user's crypto holdings, to the portfolio screen.

---
+++ Cryptopia/Cryptopia/Models/CryptoHoldingCell.swift
@@ -0,0 +1,24 @@
+//
+//  CryptoHoldingTableViewCell.swift
+//  Cryptopia
+//
+//  Created by Aryan Nateq on 21/5/19.
+//  Copyright © 2019 UTS_iOSDev. All rights reserved.
+//
+
+import UIKit
+
+class CryptoHoldingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
