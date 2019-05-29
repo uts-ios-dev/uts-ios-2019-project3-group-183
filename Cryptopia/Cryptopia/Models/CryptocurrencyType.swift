2ff3501cebb9ed1d1a3f95ae7ba97e5d79755104
richardjchris <richard.jchristiansen@gmail.com>
2019-05-29 13:24:45 +1000

    Added struct and table view IB Outlet

---
+++ Cryptopia/Cryptopia/Models/CryptocurrencyType.swift
@@ -0,0 +1,18 @@
+//
+//  CryptocurrencyType.swift
+//  Cryptopia
+//
+//  Created by Richard Christiansen on 29/5/19.
+//  Copyright © 2019 UTS_iOSDev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CryptocurrencyType {
+    let image: UIImage
+    let name: String
+    let price: String
+    let priceGrowth: Double
+    let worthInAUD: Double
+}
--- Cryptopia/Cryptopia/View Controllers/PortfolioViewController.swift
+++ Cryptopia/Cryptopia/View Controllers/PortfolioViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class PortfolioViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
