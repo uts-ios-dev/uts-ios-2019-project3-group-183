54a1988e5edbd0b575fc8200d6bb6569d799b7b7
richardjchris <richard.jchristiansen@gmail.com>
2019-06-05 10:30:53 +1000

    Changed the data source for tableView in Portfolio VC

---
+++ Cryptopia/Cryptopia/Utils/CurrencyManager.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyManager.swift
+//  Cryptopia
+//
+//  Created by Richard James on 5/6/19.
+//  Copyright © 2019 UTS_iOSDev. All rights reserved.
+//
+
+import Foundation
+
+//var currencyManager = CurrencyManager()
+
+//struct CurrencyManager: Codable {
+//    func loadPrice(_priceData) throws -> [CurrencyData] {
+//        let dataContent = try readData(from: priceData)
+//        if let scoreEntry = try? JSONDecoder().decode([CurrencyData].self, from: dataContent) {
+//            return scoreEntry
+//        }
+//        throw DataError.dataNotFound
+//    }
+//}
--- Cryptopia/Cryptopia/Views/PortfolioViewController.swift
+++ Cryptopia/Cryptopia/Views/PortfolioViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class PortfolioViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var name = ["Bitcoin", "Litecoin"]
-    var price = ["0.18 BTC", "0.1 LTC"]
-    var priceGrowth = [111, 123]
-    var worthinAUD = [1111, 14124]
+    let name = ["Bitcoin", "Litecoin", "Ethereum"]
+    var price: [Float] = [0.18, 0.10, 0.50]
+    let currencyAcronym = ["BTC", "LTC", "ETH"]
+    var priceGrowth: [Double] = [0.00, 0.00, 0.00]
+    var worthinAUD: [Double] = [0.00, 0.00, 0.00]
+    var dataArray1:JSON?
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,12 +25,47 @@ class PortfolioViewController: UIViewController, UITableViewDelegate, UITableVie
         super.viewDidLoad()
         self.tableView.dataSource = self
         self.tableView.delegate = self
-        // Do any additional setup after loading the view.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        tableView.reloadData()
-    }
+        
+        Alamofire.request("https://api.coincap.io/v2/assets").responseJSON {
+            (responseData) -> Void in
+            //Updates currency
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                let jsonObj:JSON = swiftyJsonVar["data"];
+                self.dataArray1 = jsonObj
+                
+                for index in 0...self.name.count-1 {
+                    
+                    var dataIndex: Int = 0
+        
+                    for i in 0...self.dataArray1!.count-1 {
+                        let indexedString = self.dataArray1![i]["name"].string
+                        
+                        if indexedString == self.name[index] {
+                                dataIndex = i
+                            //If indexed string is the same as the current currency name index, changes the value of dataIndex to the index of the indexedString.
+                        }
+                    }
+                    
+                    let data:JSON = self.dataArray1![dataIndex];
+                    
+                    let usdPrice = data["priceUsd"].string ?? "0.00"
+                    let USDPriceFloat = Float(usdPrice) //Converts the string to a float
+                    let worthInUSD = self.price[index] * USDPriceFloat!
+                    
+                    let change:String = data["changePercent24Hr"].string!
+                    let changeFloat = Float(change)!
+                    
+                    let AUDWorth = Double(round(100*Double(worthInUSD) * 1.43)/100) //Converts to AUD
+                    self.worthinAUD[index] = AUDWorth
+                    
+                    let priceGrowthInAUD =  Double(round(100*Double(self.worthinAUD[index] * Double(changeFloat)))/100)
+                    self.priceGrowth[index] = priceGrowthInAUD
+                    }
+                }
+                self.tableView.reloadData()
+            }
+        }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -39,23 +78,23 @@ class PortfolioViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let crypto = tableView.dequeueReusableCell(withIdentifier: "CryptocurrencyCell", for: indexPath)
+        
         let currencyNameLabel: UILabel = crypto.viewWithTag(1) as! UILabel
         let currencyPriceLabel: UILabel = crypto.viewWithTag(2) as! UILabel
         let priceGrowthLabel: UILabel = crypto.viewWithTag(3) as! UILabel
         let worthinAUDLabel: UILabel = crypto.viewWithTag(4) as! UILabel
         
         currencyNameLabel.text = name[indexPath.item]
-        currencyPriceLabel.text = price[indexPath.item]
+        currencyPriceLabel.text = "\(price[indexPath.item]) \(currencyAcronym[indexPath.item])"
         if (priceGrowth[indexPath.item] > 0) {
             priceGrowthLabel.text = "▲ $AU\(priceGrowth[indexPath.item])"
             priceGrowthLabel.textColor = UIColor.init(red:0.01, green:0.81, blue:0.00, alpha:1.0)
         } else if (priceGrowth[indexPath.item] < 0) {
-            priceGrowthLabel.text = "▼ $AU\(priceGrowth[indexPath.item])"
-             priceGrowthLabel.textColor = UIColor.init(red:0.76, green:0.23, blue:0.13, alpha:1.0)
+            priceGrowthLabel.text = "▼ $AU \(priceGrowth[indexPath.item])"
+            priceGrowthLabel.textColor = UIColor.init(red:0.76, green:0.23, blue:0.13, alpha:1.0)
         }
         
         worthinAUDLabel.text = "$AU \(worthinAUD[indexPath.item])"
-        
         return crypto
     }
     
